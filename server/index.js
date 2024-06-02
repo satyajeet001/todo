2c934a3c50891bad4d6f9c00823d8f6932bb2f08
Satyajeet Das <99283330+satyajeet001@users.noreply.github.com>
2024-06-02 19:32:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 6010;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://todo-sigma-liart.vercel.app/"],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
